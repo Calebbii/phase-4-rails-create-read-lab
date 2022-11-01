ddfef69978fe1927a25ec5ddcdaf2161350bbbc7
Calebbii <biicaleb316@gmail.com>
2022-11-01 19:11:06 +0300

    lab updated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
 
+  def show
+    # params[:id] refers to the dynamic part of our route, defined by :id
+    # a request to /birds/2 would give params[:id] a value of 2
+    bird = Bird.find_by(id: params[:id])
+    render json: bird
+  end
+
+  def create
+    bird = Bird.create(name: params[:name], species: params[:species])
+    render json: bird, status: :created
+  end
+
 end
